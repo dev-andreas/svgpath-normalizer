f5214bcc633a104b4f0289b6fcbdd18ff32ec21e
Andreas Gerasimow <andreas.gerasimow12@gmail.com>
2021-02-02 21:55:24 +0100

    Put everything into class

--- example.py
+++ example.py
@@ -1,12 +1,12 @@
 import svgpathnormalizer as sn
 
 path = 'M 100 300 Q 150 50 200 300 Q 250 550 300 300 Q 350 50 400 300 C 450 550 450 50 500 300 C 550 50 550 550 600 300 A 50 50 0 1 1 700 300 Z'
-normalized = sn.normalize(path)
 
-print('NORMALIZED')
-print(normalized)
+normalizer = sn.SVGNormalizer()
 
-scaled = sn.normalize(path, scalar=3)
+scaled = normalizer.scale(path, scalar=3)
 
 print('SCALED')
 print(scaled)
+print('ALSO POSSIBLE')
+print(normalizer.prev_scaled_path)
--- svgpathnormalizer.py
+++ svgpathnormalizer.py
@@ -1,120 +1,134 @@
 '''Module for scaling and svgpaths'''
+class SVGNormalizer(object):
 
-def scale(svgpath, scalar=1):
-    tokens = svgpath.split()
-    points = init_points(tokens)
-    del(tokens)
-    largest_number = get_largest_number(points)
-
-    for point in points:
-        if point[0].lower() == 'm':
-            point = l_normalizer(point, largest_number, scalar)
-        if point[0].lower() == 'l':
-            point = l_normalizer(point, largest_number, scalar)
-        elif point[0].lower() == 'q':
-            point = q_normalizer(point, largest_number, scalar)
-        elif point[0].lower() == 'c':
-            point = c_normalizer(point, largest_number, scalar)
-        elif point[0].lower() == 'a':
-            point = a_normalizer(point, largest_number, scalar)
-
-    svgpath = ''
+    def __init__(self):
+        self.__prev_scaled_path = None
     
-    for point in points:
-        for token in point:
-            svgpath += token + ' '
-    return svgpath
+    def scale(self, svgpath, scalar: float=1) -> str:
+        tokens = svgpath.split()
+        points = self.__init_points(tokens)
+        del(tokens)
+        largest_number = self.__get_largest_number(points)
 
-def l_normalizer(point, largest_number, scalar):
-    x = float(point[1])
-    y = float(point[2])
+        for point in points:
+            if point[0].lower() == 'm':
+                point = self.__l_normalizer(point, largest_number, scalar)
+            if point[0].lower() == 'l':
+                point = self.__l_normalizer(point, largest_number, scalar)
+            elif point[0].lower() == 'q':
+                point = self.__q_normalizer(point, largest_number, scalar)
+            elif point[0].lower() == 'c':
+                point = self.__c_normalizer(point, largest_number, scalar)
+            elif point[0].lower() == 'a':
+                point = self.__a_normalizer(point, largest_number, scalar)
 
-    x /= (largest_number / scalar)
-    y /= (largest_number / scalar)
-
-    point[1] = str(x)
-    point[2] = str(y)
-    return point
-
-def q_normalizer(point, largest_number, scalar):
-    point = l_normalizer(point, largest_number, scalar)
-
-    c_x = float(point[3])
-    c_y = float(point[4])
-
-    c_x /= (largest_number / scalar)
-    c_y /= (largest_number / scalar)
-
-    point[3] = str(c_x)
-    point[4] = str(c_y)
-    return point
-
-def c_normalizer(point, largest_number, scalar):
-    point = q_normalizer(point, largest_number, scalar)
-    c_x = float(point[5])
-    c_y = float(point[6])
-
-    c_x /= (largest_number / scalar)
-    c_y /= (largest_number / scalar)
-
-    point[5] = str(c_x)
-    point[6] = str(c_y)
-    return point
-
-def a_normalizer(point, largest_number, scalar):
-    point = l_normalizer(point, largest_number, scalar)
-
-    p_x = float(point[6])
-    p_y = float(point[7])
-
-    p_x /= (largest_number / scalar)
-    p_y /= (largest_number / scalar)
-
-    point[6] = str(p_x)
-    point[7] = str(p_y)
-    return point
-
-def init_points(tokens):
-    points = []
-    point = []
-
-    current_type = tokens[0]
-    point.append(tokens[0])
-
-    for token in tokens[1:]:
-        if is_number(token):
-            point.append(token)
-        else:
-            points.append(point)
-            point = []
-            point.append(token)
-    points.append(point)
-    return points
-
-def get_largest_number(points):
-    largest_number = 0
-    for point in points:
-        number1, number2 = 0, 0
+        svgpath = ''
         
-        if point[0].lower() == 'z':
-            continue
-        elif point[0].lower() ==  'a':
-            number1 = float(point[6])
-            number2 = float(point[7])
-        else:
-            number1 = float(point[1])
-            number2 = float(point[2])
-        
-        if number1 > largest_number:
-            largest_number = number1
-        if number2 > largest_number:
-            largest_number = number2
-    return largest_number
+        for point in points:
+            for token in point:
+                svgpath += token + ' '
+        self.__prev_scaled_path = svgpath
+        return svgpath
 
-def is_number(s):
-    try:
-        float(s)
-        return True
-    except ValueError:
-        return False
-    
+    def __l_normalizer(self, point: list, largest_number: float, scalar: float) -> list:
+        x = float(point[1])
+        y = float(point[2])
+
+        x /= (largest_number / scalar)
+        y /= (largest_number / scalar)
+
+        point[1] = str(x)
+        point[2] = str(y)
+        return point
+
+    def __q_normalizer(self, point: list, largest_number: float, scalar: float) -> list:
+        point = self.__l_normalizer(point, largest_number, scalar)
+
+        c_x = float(point[3])
+        c_y = float(point[4])
+
+        c_x /= (largest_number / scalar)
+        c_y /= (largest_number / scalar)
+
+        point[3] = str(c_x)
+        point[4] = str(c_y)
+        return point
+
+    def __c_normalizer(self, point: list, largest_number: float, scalar: float) -> list:
+        point = self.__q_normalizer(point, largest_number, scalar)
+        c_x = float(point[5])
+        c_y = float(point[6])
+
+        c_x /= (largest_number / scalar)
+        c_y /= (largest_number / scalar)
+
+        point[5] = str(c_x)
+        point[6] = str(c_y)
+        return point
+
+    def __a_normalizer(self, point: list, largest_number: float, scalar: float) -> list:
+        point = self.__l_normalizer(point, largest_number, scalar)
+
+        p_x = float(point[6])
+        p_y = float(point[7])
+
+        p_x /= (largest_number / scalar)
+        p_y /= (largest_number / scalar)
+
+        point[6] = str(p_x)
+        point[7] = str(p_y)
+        return point
+
+    def __init_points(self, tokens: list) -> list:
+        points = []
+        point = []
+
+        current_type = tokens[0]
+        point.append(tokens[0])
+
+        for token in tokens[1:]:
+            if self.__is_number(token):
+                point.append(token)
+            else:
+                points.append(point)
+                point = []
+                point.append(token)
+        points.append(point)
+        return points
+
+    def __get_largest_number(self, points: list) -> float:
+        largest_number = 0
+        for point in points:
+            number1, number2 = 0, 0
+            
+            if point[0].lower() == 'z':
+                continue
+            elif point[0].lower() ==  'a':
+                number1 = float(point[6])
+                number2 = float(point[7])
+            else:
+                number1 = float(point[1])
+                number2 = float(point[2])
+            
+            if number1 > largest_number:
+                largest_number = number1
+            if number2 > largest_number:
+                largest_number = number2
+        return largest_number
+
+    def __is_number(self, s: str) -> bool:
+        try:
+            float(s)
+            return True
+        except ValueError:
+            return False
+
+    # GETTERS AND SETTERS
+
+    def __get_prev_scaled_path(self) -> str:
+        return self.__prev_scaled_path
+
+    # PROPERTIES
+
+    prev_scaled_path: str = property(__get_prev_scaled_path, None)
+        
